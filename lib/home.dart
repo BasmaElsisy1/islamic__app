d47da6838b584d617729b7a710ad68a3cbc1269e
basmaelsisy <basma.el.sisy@outlook.com>
2022-11-26 10:30:54 +0200

    navigation bar

--- lib/home.dart
+++ lib/home.dart
@@ -5,10 +5,41 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Islami'),
-      ),
+    return Stack(
+      children: [
+        Image.asset(
+          'assets/images/background.png',
+          width: double.infinity,
+          fit: BoxFit.fill,
+        ),
+        Scaffold(
+          appBar: AppBar(
+            centerTitle: true,
+            title: Text('Islami', style: Theme.of(context).textTheme.headline1),
+          ),
+          bottomNavigationBar: BottomNavigationBar(
+            items: [
+              BottomNavigationBarItem(
+                icon: ImageIcon(AssetImage('assets/images/icon_radio.png'),
+                  size: 48,),
+                label: 'radio',
+              ),
+              BottomNavigationBarItem(
+                icon: Image.asset('assets/images/background.png'),
+                label: '',
+              ),
+              BottomNavigationBarItem(
+                icon: Image.asset('assets/images/icon_sebha.png'),
+                label: '',
+              ),
+              BottomNavigationBarItem(
+                icon: Image.asset('assets/images/radio.png'),
+                label: '',
+              ),
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:islamic/theme_details.dart';
 import 'home.dart';
 
 void main() {
@@ -9,8 +10,12 @@ class MyApplication extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       initialRoute: HomeScreen.routeName,
       routes: {HomeScreen.routeName: (context) => HomeScreen()},
+      theme: ThemeDetails.lightTheme,
+      darkTheme: ThemeDetails.darkTheme,
+      themeMode: ThemeMode.light,
     );
   }
 }
