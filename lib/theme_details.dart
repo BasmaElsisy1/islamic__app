20412f0e6fb7ade21a811bca6146566baaf23681
basmaelsisy <basma.el.sisy@outlook.com>
2022-11-26 16:35:18 +0200

    localization

--- lib/theme_details.dart
+++ lib/theme_details.dart
@@ -5,19 +5,18 @@ class ThemeDetails {
   static const GoldColor = Color.fromRGBO(183, 147, 95, 1.0);
   static ThemeData lightTheme = ThemeData(
       scaffoldBackgroundColor: Colors.transparent,
-      appBarTheme:
-          AppBarTheme(backgroundColor: Colors.transparent, elevation: 0.0,
-              titleTextStyle:TextStyle(
-                  fontSize: 30,
-                  color: blackColor,
-                  fontWeight: FontWeight.bold
-              ) ),
+      appBarTheme: AppBarTheme(
+          backgroundColor: Colors.transparent,
+          elevation: 0.0,
+          titleTextStyle: TextStyle(
+              fontSize: 30, color: blackColor, fontWeight: FontWeight.bold)),
       textTheme: TextTheme(
           headline1: TextStyle(
-        fontSize: 30,
-        color: blackColor,
-            fontWeight: FontWeight.bold
-      )));
+              fontSize: 30, color: blackColor, fontWeight: FontWeight.bold)),
+      bottomNavigationBarTheme: BottomNavigationBarThemeData(
+          backgroundColor: GoldColor,
+          selectedItemColor: blackColor,
+          unselectedItemColor: Colors.white));
   static ThemeData darkTheme = ThemeData(
       scaffoldBackgroundColor: Colors.transparent,
       appBarTheme: AppBarTheme(backgroundColor: Colors.transparent));
